ebc177d01ae4112920e3301e458383562c2f07a2
2936410488 <132274351+2936410488@users.noreply.github.com>
2025-05-16 23:48:24 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -52,11 +52,11 @@ sync_repo() {
 }
 
 if [[ $1 = "-u" || $1 = "--update" ]]; then
-    sync_repo $AK3_DIR "https://github.com/2936410488/AnyKernel3.git" "Gilverly" true
+    sync_repo $AK3_DIR "https://github.com/2936410488/AnyKernel3.git" "master" true
     sync_repo $TC_DIR "https://bitbucket.org/rdxzv/clang-standalone.git" "20" true
 	exit
 else
-    sync_repo $AK3_DIR "https://github.com/2936410488/AnyKernel3.git" "Gilverly" false
+    sync_repo $AK3_DIR "https://github.com/2936410488/AnyKernel3.git" "master" false
     sync_repo $TC_DIR "https://bitbucket.org/rdxzv/clang-standalone.git" "20" false
 fi
 
